26c3d31b6e7ee14a88059698e56c5951d6e3f204
chrisheo <jftj97@gmail.com>
2022-09-03 13:35:48 +0900

    add imageupload module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RecipesModule } from './recipes/recipes.module';
 import { ConfigModule } from '@nestjs/config';
+import {ImageUploadModule} from "./imageupload/imageupload.module";
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
     MongooseModule.forRoot(
     process.env.MONGODB_URL
-  ), RecipesModule],
+  ), RecipesModule, ImageUploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
